d2604e240cda95b7e75634d80f9e81a4f23b2d47
MonsterDruide1 <5958456@gmail.com>
2024-05-31 12:46:32 +0200

    missing.h: Externalize to `sead`

--- src/stubs/missing.h
+++ src/stubs/missing.h
@@ -1,7 +1,3 @@
 #pragma once
 
-#include <cstdio>
-
-#define DEREF_NULL *(volatile int*)0;
-#define WARN_UNIMPL printf("Function not implemented: %s (%s:%d)\n", __func__, __FILE__, __LINE__)
-#define CRASH {WARN_UNIMPL;DEREF_NULL}
+#include <basis/seadTypes.h>
