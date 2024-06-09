aad3a3f9043c59fd8ace5abc09d82766f0979404
MonsterDruide1 <5958456@gmail.com>
2024-06-09 18:47:59 +0200

    implement more collision-related functions

--- src/stubs/game-ignored-other.cpp
+++ src/stubs/game-ignored-other.cpp
@@ -6,6 +6,8 @@ namespace rs {
 // used too little to be worth implementing here (KinopioTent, Stand, Trampoline, Parasol and a few others)
 bool isTouchJumpCode(al::LiveActor const*, IUsePlayerCollision const*) { return false; }
 
+void addPlayerJumpCount(const al::LiveActor*) {}
+
 }
 
 PlayerSandSinkAffect::PlayerSandSinkAffect(al::LiveActor const*, PlayerConst const*, PlayerInput const*, IUsePlayerCollision*, PlayerEffect*) {}
--- src/stubs/unknown-collision.cpp
+++ src/stubs/unknown-collision.cpp
@@ -3,12 +3,9 @@
 #include "Stuff.h"
 #include "Util/PlayerCollisionUtil.h"
 
-bool rs::calcSlideDir(sead::Vector3<float>*, sead::Vector3<float> const&, sead::Vector3<float> const&) { WARN_UNIMPL;return false; }
-f32 rs::getGroundHeight(const IUsePlayerHeightCheck*) { WARN_UNIMPL;return 0; }
-void rs::startHitReactionLandJumpIfLanding(al::LiveActor const*, IUsePlayerCollision const*, bool) { WARN_UNIMPL; }
-void rs::noticePlayerJumpStart(PlayerTrigger*, al::LiveActor const*) { WARN_UNIMPL; }
+void rs::startHitReactionLandJumpIfLanding(al::LiveActor const*, IUsePlayerCollision const*, bool) { CRASH; }
+
 bool rs::tryFindSnapMoveAreaDir(sead::Vector3<float>*, al::LiveActor const*, IUsePlayerCollision const*) { WARN_UNIMPL;return false; }
-void rs::resetCollisionPose(const IUsePlayerCollision *, const sead::Quat<float> &) { WARN_UNIMPL; }
 void rs::calcSnapVelocitySnapMoveAreaWithCutDir(sead::Vector3<float>* result, al::LiveActor const*, IUsePlayerCollision const*, sead::Vector3<float> const& vel, float, sead::Vector3<float> const&) { WARN_UNIMPL;*result=vel; }
 void rs::calcSnapVelocitySnapMoveArea(sead::Vector3<float>* result, al::LiveActor const*, IUsePlayerCollision const*, sead::Vector3<float> const& vel, float) { WARN_UNIMPL;*result=vel; }
 
