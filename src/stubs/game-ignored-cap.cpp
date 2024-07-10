ed0a9de1bd5a12d74e8d144ef4102b32425a6c3b
MonsterDruide1 <5958456@gmail.com>
2024-07-10 17:46:34 +0200

    Change `cap` to ignored

--- src/stubs/game-ignored-cap.cpp
+++ src/stubs/game-ignored-cap.cpp
@@ -9,99 +9,60 @@ HackCap::HackCap(al::LiveActor const*, char const*, PlayerInput const*, PlayerAr
                  IUsePlayerCollision const*, IUsePlayerHeightCheck const*, PlayerWetControl const*,
                  PlayerJointControlKeeper const*, HackCapJudgePreInputSeparateThrow*,
                  HackCapJudgePreInputSeparateJump*)
-    : al::LiveActor("") {
-    WARN_UNIMPL;
-}
-void HackCap::updateSeparateMode(PlayerSeparateCapFlag const*) {
-    WARN_UNIMPL;
-}
+    : al::LiveActor("") {}
+void HackCap::updateSeparateMode(PlayerSeparateCapFlag const*) {}
 bool HackCap::isNoPutOnHide() {
-    WARN_UNIMPL;
     return false;
 }
 bool HackCap::isEnableSpinAttack() {
-    WARN_UNIMPL;
     return true;
 }
 bool HackCap::requestReturn(bool*) {
-    WARN_UNIMPL;
     return false;
 }
 bool HackCap::isRequestableReturn() const {
-    WARN_UNIMPL;
     return false;
 }
 bool HackCap::isEnablePreInput() const {
-    WARN_UNIMPL;
     return false;
 }
-void HackCap::startCatch(char const*, bool, sead::Vector3<float> const&) {
-    WARN_UNIMPL;
-}
-void HackCap::prepareCooperateThrow() {
-    WARN_UNIMPL;
-}
+void HackCap::startCatch(char const*, bool, sead::Vector3<float> const&) {}
+void HackCap::prepareCooperateThrow() {}
 bool HackCap::isEnableThrowSeparate() const {
-    WARN_UNIMPL;
     return true;
 }
 void HackCap::startThrowSeparatePlayJump(sead::Vector3<float> const&, sead::Vector3<float> const&,
-                                         float) {
-    WARN_UNIMPL;
-}
+                                         float) {}
 void HackCap::startThrowSeparatePlay(sead::Vector3<float> const&, sead::Vector3<float> const&,
-                                     float, bool) {
-    WARN_UNIMPL;
-}
+                                     float, bool) {}
 f32 HackCap::calcSeparateHideSpeedH(sead::Vector3<float> const&) {
-    WARN_UNIMPL;
     return 0.0f;
 }
-void HackCap::startSpinAttack(const sead::SafeString&) {
-    WARN_UNIMPL;
-}
+void HackCap::startSpinAttack(const sead::SafeString&) {}
 bool HackCap::cancelCapState() {
-    WARN_UNIMPL;
     return false;
 }
 bool HackCap::isSpinAttack() {
-    WARN_UNIMPL;
     return false;
 }
 void HackCap::startThrow(bool, sead::Vector3<float> const&, sead::Vector3<float> const&, float,
                          sead::Vector2<float> const&, sead::Vector2<float> const&,
                          sead::Vector3<float> const&, bool, sead::Vector3<float> const&,
-                         HackCap::SwingHandType, bool, float, int) {
-    WARN_UNIMPL;
-}
-void HackCap::attackSpin(al::HitSensor*, al::HitSensor*, float) {
-    WARN_UNIMPL;
-}
+                         HackCap::SwingHandType, bool, float, int) {}
+void HackCap::attackSpin(al::HitSensor*, al::HitSensor*, float) {}
 
 bool PlayerInput::isHoldCapSeparateJump() const {
-    WARN_UNIMPL;
     return false;
 }
 
-PlayerSeparateCapFlag::PlayerSeparateCapFlag() {
-    WARN_UNIMPL;
-}
+PlayerSeparateCapFlag::PlayerSeparateCapFlag() {}
 
-void CapFunction::putOnCapPlayer(HackCap*, PlayerAnimator*) {
-    WARN_UNIMPL;
-}
+void CapFunction::putOnCapPlayer(HackCap*, PlayerAnimator*) {}
 
-PlayerJudgeCapCatch::PlayerJudgeCapCatch(al::LiveActor const*, PlayerCounterAfterCapCatch const*) {
-    WARN_UNIMPL;
-}
-void PlayerJudgeCapCatch::reset() {
-    WARN_UNIMPL;
-}
-void PlayerJudgeCapCatch::update() {
-    WARN_UNIMPL;
-}
+PlayerJudgeCapCatch::PlayerJudgeCapCatch(al::LiveActor const*, PlayerCounterAfterCapCatch const*) {}
+void PlayerJudgeCapCatch::reset() {}
+void PlayerJudgeCapCatch::update() {}
 bool PlayerJudgeCapCatch::judge() const {
-    WARN_UNIMPL;
     return false;
 }
 
@@ -109,72 +70,44 @@ PlayerJudgeCapCatchPop::PlayerJudgeCapCatchPop(al::LiveActor const*, PlayerInput
                                                IUsePlayerCollision const*,
                                                IPlayerModelChanger const*,
                                                PlayerCapActionHistory const*,
-                                               PlayerCounterAfterCapCatch const*) {
-    WARN_UNIMPL;
-}
-void PlayerJudgeCapCatchPop::reset() {
-    WARN_UNIMPL;
-}
-void PlayerJudgeCapCatchPop::update() {
-    WARN_UNIMPL;
-}
+                                               PlayerCounterAfterCapCatch const*) {}
+void PlayerJudgeCapCatchPop::reset() {}
+void PlayerJudgeCapCatchPop::update() {}
 bool PlayerJudgeCapCatchPop::judge() const {
-    WARN_UNIMPL;
     return false;
 }
 
 HackCapJudgePreInputHoveringJump::HackCapJudgePreInputHoveringJump(al::LiveActor const*,
                                                                    IUsePlayerCollision const*,
                                                                    HackCap const*,
-                                                                   PlayerInput const*) {
-    WARN_UNIMPL;
-}
-void HackCapJudgePreInputHoveringJump::reset() {
-    WARN_UNIMPL;
-}
-void HackCapJudgePreInputHoveringJump::update() {
-    WARN_UNIMPL;
-}
+                                                                   PlayerInput const*) {}
+void HackCapJudgePreInputHoveringJump::reset() {}
+void HackCapJudgePreInputHoveringJump::update() {}
 bool HackCapJudgePreInputHoveringJump::judge() const {
-    WARN_UNIMPL;
     return false;
 }
 
 HackCapJudgeHoldHoveringJump::HackCapJudgeHoldHoveringJump(al::LiveActor const*,
                                                            PlayerColliderHakoniwa const*,
                                                            HackCap const*, PlayerInput const*,
-                                                           PlayerConst const*) {
-    WARN_UNIMPL;
-}
-void HackCapJudgeHoldHoveringJump::reset() {
-    WARN_UNIMPL;
-}
-void HackCapJudgeHoldHoveringJump::update() {
-    WARN_UNIMPL;
-}
+                                                           PlayerConst const*) {}
+void HackCapJudgeHoldHoveringJump::reset() {}
+void HackCapJudgeHoldHoveringJump::update() {}
 bool HackCapJudgeHoldHoveringJump::judge() const {
-    WARN_UNIMPL;
     return false;
 }
 
 HackCapJudgePreInputSeparateThrow::HackCapJudgePreInputSeparateThrow(PlayerInput const*,
                                                                      PlayerSeparateCapFlag const*) {
-    WARN_UNIMPL;
+
 }
-void HackCapJudgePreInputSeparateThrow::reset() {
-    WARN_UNIMPL;
-}
-void HackCapJudgePreInputSeparateThrow::update() {
-    WARN_UNIMPL;
-}
+void HackCapJudgePreInputSeparateThrow::reset() {}
+void HackCapJudgePreInputSeparateThrow::update() {}
 bool HackCapJudgePreInputSeparateThrow::judge() const {
-    WARN_UNIMPL;
     return false;
 }
 
 PlayerStateCapCatchPop::PlayerStateCapCatchPop(al::LiveActor*, PlayerConst const*,
                                                IUsePlayerCollision const*, PlayerInput const*,
                                                PlayerAnimator*, HackCap*)
-    : al::NerveStateBase("") {
-    WARN_UNIMPL;
-}
+    : al::NerveStateBase("") {}
