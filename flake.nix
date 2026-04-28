23222bf4cf6f6768c02fd26287fad80bf0312c8c
MonsterDruide1 <5958456@gmail.com>
2026-04-28 04:27:15 +0200

    setup aarch64 cross compiling

--- flake.nix
+++ flake.nix
@@ -18,13 +18,44 @@
       _module.args.pkgs = import inputs.nixpkgs {
         config.allowUnfree = true;
       };
-      devShells.default = pkgs.mkShell rec {
-        buildInputs = with pkgs; [
+      devShells.default = pkgs.pkgsCross.aarch64-multiplatform.stdenv.mkDerivation {
+    name = "cross-dev";
+        nativeBuildInputs = with pkgs; [
           cmake
           ninja
-          clang
-          pkg-config
-          libx11
+          rustToolchain
+          (python313.withPackages (python-pkgs: [
+            python-pkgs.toml
+            python-pkgs.gymnasium
+          ]))
+        ];
+
+        buildInputs = with pkgs; [
+          pkgsCross.aarch64-multiplatform.libx11
+          pkgsCross.aarch64-multiplatform.libXrandr
+          pkgsCross.aarch64-multiplatform.libXinerama
+          pkgsCross.aarch64-multiplatform.libXcursor
+          pkgsCross.aarch64-multiplatform.libXi
+          pkgsCross.aarch64-multiplatform.libffi
+          pkgsCross.aarch64-multiplatform.openssl
+          pkgsCross.aarch64-multiplatform.libGL
+          pkgsCross.aarch64-multiplatform.wayland
+          pkgsCross.aarch64-multiplatform.libxkbcommon
+          pkgsCross.aarch64-multiplatform.glibc
+          pkgsCross.aarch64-multiplatform.linuxHeaders
+        ];
+
+        shellHook = ''
+          unset LD_LIBRARY_PATH
+        '';
+        /*buildInputs = with pkgs; [
+          cmake
+          ninja
+          #clang
+          #llvmPackages_20.clang-unwrapped
+          pkgsCross.aarch64-multiplatform.pkg-config
+          pkgsCross.aarch64-multiplatform.binutils
+          pkgsCross.aarch64-multiplatform.libx11
           libxrandr
           libxinerama
           libxcursor
@@ -43,9 +74,15 @@
           openssl
           libclang
           ncurses5
+
+          pkgsCross.aarch64-multiplatform.buildPackages.gcc
+          pkgsCross.aarch64-multiplatform.glibc
+          #pkgsCross.aarch64-multiplatform.stdenv
+          pkgsCross.aarch64-multiplatform.glibc.dev
+          pkgsCross.aarch64-multiplatform.linuxHeaders
         ];
         LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
-        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";*/
       };
     };
   };
