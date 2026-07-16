9efba24268525725fa788de0f1206eb0eb0865c2
MonsterDruide1 <5958456@gmail.com>
2026-07-17 01:20:23 +0200

    more functions implemented

--- src/stubs/game-ignored-push.cpp
+++ src/stubs/game-ignored-push.cpp
@@ -1,14 +1,7 @@
 #include "Library/Collision/CollisionParts.h"
 #include "Library/Collision/CollisionUtil.h"
 #include "PlayerUtil.h"
-
-PlayerExternalVelocity::PlayerExternalVelocity(al::LiveActor const*, IUsePlayerCollision const*,
-                                               sead::Vector3<float> const*) {}
-void PlayerExternalVelocity::requestApplyLastGroundInertia() {}
-void PlayerExternalVelocity::cancelAndFeedbackLastGroundInertia(al::LiveActor*, float, bool) {}
-void PlayerExternalVelocity::update() {}
-bool PlayerExternalVelocity::isExistForce() const {return false;}
-bool PlayerExternalVelocity::isExistSnapForce() const {return false;}
+#include "Util/AreaUtil.h"
 
 PlayerBindKeeper::PlayerBindKeeper(al::HitSensor*, IUsePlayerPuppet*) {}
 void PlayerBindKeeper::clearBindableSensor() {}
@@ -17,3 +10,6 @@ bool PlayerBindKeeper::sendStartMsg() {return false;}
 void al::CollisionParts::calcForceMovePower(sead::Vector3f* a1, const sead::Vector3f&) const {*a1 = {0.0f, 0.0f, 0.0f};}
 
 bool alCollisionUtil::isCollisionMoving(const al::HitInfo*) {return false;}
+
+bool rs::calcExtForceAreaVelocity(sead::Vector3<float>*, al::IUseAreaObj const*, sead::Vector3<float> const&, sead::Vector3<float> const&, sead::Vector3<float> const&) {return false;}
+bool al::calcForceMovePowerExceptNormal(sead::Vector3<float>*, al::CollisionParts const*, sead::Vector3<float> const&, sead::Vector3<float> const&) {return false;}
